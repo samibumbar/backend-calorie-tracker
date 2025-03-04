c57ad966da23daa3c4dfb37e20d3e9092a8615d4
OMBRENORMAL <134549221+samibumbar@users.noreply.github.com>
2025-03-04 10:17:12 +0100

    Remove tracked files now ignored by .gitignore

--- dist/routes/products.routes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.productsRoutes = void 0;
-const express_1 = require("express");
-const controllers_1 = require("../controllers");
-const router = (0, express_1.Router)();
-router.get("/search", controllers_1.searchProducts);
-exports.productsRoutes = router;
--- dist/services/calorieCalculator.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.calculateDailyCalories = void 0;
-const calculateDailyCalories = (weight, height, age, desiredWeight) => {
-    return (10 * weight + 6.25 * height - 5 * age - 161 - 10 * (weight - desiredWeight));
-};
-exports.calculateDailyCalories = calculateDailyCalories;
